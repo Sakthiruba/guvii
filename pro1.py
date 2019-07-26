046c5d0a5e55ffca7f942681a0384c57a16620b6
Sakthiruba <51356091+Sakthiruba@users.noreply.github.com>
2019-07-26 06:57:55 -0700

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -1,1 +1,12 @@
-print(" ")
+mo=int(input())
+q1=[]
+for i in range(0,mo):
+  r1=input()
+  q1.append(r1)
+s1=[]
+for i in zip(*q1):
+  if(i.count(i[0])==len(i)):
+    s1.append(i[0])
+  else:
+    break
+print(''.join(s1))  
